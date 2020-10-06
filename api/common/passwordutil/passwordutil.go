86f48722638655c0edddb337f8aecf2b49d3aeda
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-06 16:28:22 +0300

    password hashing utility

---
+++ api/common/passwordutil/passwordutil.go
@@ -0,0 +1,16 @@
+package passwordutil
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword returns a hashed password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(bytes), err
+}
+
+// CheckPasswordHash validates hashed passwords
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
