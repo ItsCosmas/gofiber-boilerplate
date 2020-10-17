6a91a3c09f4fd890c7e2cc1479c830123eae786e
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-18 01:20:24 +0300

    Added a Bio Field to User Model

--- api/models/user/user.go
+++ api/models/user/user.go
@@ -11,6 +11,7 @@ type User struct {
 	ExternalID     string `gorm:"unique;NOT NULL;"`
 	FullName       string `gorm:"NOT NULL"`
 	ProfilePicture string `gorm:"size:255"`
+	Bio            string `gorm:"size:255"`
 	Email          string `gorm:"NOT NULL;UNIQUE"`
 	Password       string `gorm:"NOT NULL"`
 	Role           string `gorm:"NOT NULL;size:255;DEFAULT:'standard'"`
