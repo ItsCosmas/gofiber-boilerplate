81501fc9682331f71c9a78c371e8f3c9c020e201
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-18 01:20:07 +0300

    Add GetByEmail to get user with given mail

--- api/repositories/user/user.go
+++ api/repositories/user/user.go
@@ -12,3 +12,12 @@ import (
 func Create(user *user.User) error {
 	return db.PgDB.Create(user).Error
 }
+
+// GetByEmail gets user with the given email
+func GetByEmail(email string) (*user.User, error) {
+	var user user.User
+	if err := db.PgDB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
