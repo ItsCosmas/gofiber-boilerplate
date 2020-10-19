d20bd92abcdb104e7350ad6c1f75f5f00bb446ae
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-20 00:50:50 +0300

    Mongo Configs

--- api/configs/mongo.go
+++ api/configs/mongo.go
@@ -1,1 +1,19 @@
 package configs
+
+import (
+	"os"
+)
+
+// MongoDBConfig object
+type MongoDBConfig struct {
+	URI         string `env:"MONGO_URI"` // i.e. "mongodb://localhost:27017"
+	MongoDBName string `env:"MONGO_DB_NAME"`
+}
+
+// GetMongoDBConfig returns MongoDBConfig object
+func GetMongoDBConfig() MongoDBConfig {
+	return MongoDBConfig{
+		URI:         os.Getenv("MONGO_URI"),
+		MongoDBName: os.Getenv("MONGO_DB_NAME"),
+	}
+}
