618803c2bcc12d4d27e84dfd05eada4b100d9954
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-20 00:53:10 +0300

    Add Mongo Instance

--- api/database/db.go
+++ api/database/db.go
@@ -3,20 +3,36 @@ package database
 import (
 
 	// Configs
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	cfg "github.com/ItsCosmas/gofiber-boilerplate/api/configs"
 
+	// Gorm
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	// Mongo
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	// DBCon is the connection handle
-	// for the database
+	// PgDB is the postgress connection handle
 	PgDB *gorm.DB
 )
+
+// MongoInstance contains the Mongo client and database objects
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+// MgDB is the mongodb connection handle
+var MgDB MongoInstance
 
 // ConnectPostgres Returns the Pg DB Instance
 func ConnectPostgres() {
@@ -24,9 +40,44 @@ func ConnectPostgres() {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		fmt.Println("Could Not Establish DB Connection")
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		fmt.Println("Could Not Establish Postgres DB Connection")
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 		log.Fatal(err)
 	}
 
+	fmt.Println("----------------------------")
+	fmt.Println("Connected To Postgres DB ...")
+	fmt.Println("----------------------------")
+
 	PgDB = db
+}
+
+// ConnectMongo Returns the Mongo DB Instance
+func ConnectMongo() {
+	client, err := mongo.NewClient(options.Client().ApplyURI(cfg.GetConfig().Mongo.URI))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(cfg.GetConfig().Mongo.MongoDBName)
+
+	if err != nil {
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		fmt.Println("Could Not Establish Mongo DB Connection")
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+		log.Fatal(err)
+	}
+
+	fmt.Println("*************************")
+	fmt.Println("Connected To Mongo DB ...")
+	fmt.Println("*************************")
+
+	MgDB = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+
 }
