3b767a3fd96b069a95833a956864e4b960578739
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-20 23:18:52 +0300

    GetAllBooks, GetBookByID, GetBookByISBN

--- api/repositories/book/book.go
+++ api/repositories/book/book.go
@@ -3,9 +3,12 @@ package book
 import (
 	// user model
 	"context"
+	"log"
 	"time"
 
 	"github.com/ItsCosmas/gofiber-boilerplate/api/models/book"
+	"go.mongodb.org/mongo-driver/bson"
+
 	// database
 	db "github.com/ItsCosmas/gofiber-boilerplate/api/database"
 )
@@ -25,13 +28,64 @@ func Create(book *book.Book) (*book.Book, error) {
 }
 
 // GetAll Returns all Books
-func GetAll() (*[]book.Book, error) {
-	return nil, nil
+func GetAll() ([]*book.Book, error) {
+	var results []*book.Book
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	collection := db.MgDB.Db.Collection("books")
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+	// bson.M
+	var b *book.Book
+	for cur.Next(ctx) {
+		err := cur.Decode(&b)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, b)
+
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return results, nil
 }
 
 // GetByID gets book with the given id
 func GetByID(id string) (*book.Book, error) {
-	return nil, nil
+	var book book.Book
+	filter := bson.M{"external_id": id}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel() // releases resources if CreateBook completes before timeout elapses
+	collection := db.MgDB.Db.Collection("books")
+
+	err := collection.FindOne(ctx, filter).Decode(&book)
+	if err != nil {
+		return nil, err
+	}
+
+	return &book, nil
+}
+
+// GetByISBN gets book with the given ISBN
+func GetByISBN(isbn string) (*book.Book, error) {
+	var book book.Book
+	filter := bson.M{"isbn": isbn}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel() // releases resources if CreateBook completes before timeout elapses
+	collection := db.MgDB.Db.Collection("books")
+
+	err := collection.FindOne(ctx, filter).Decode(&book)
+	if err != nil {
+		return nil, err
+	}
+
+	return &book, nil
 }
 
 // Update gets the book with given id and updates it
