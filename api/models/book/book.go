563ec290923cf3ec8d220c8f077771632638f80b
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2020-10-20 16:29:13 +0300

    Add New Fields to Book Model

--- api/models/book/book.go
+++ api/models/book/book.go
@@ -8,15 +8,17 @@ import (
 
 // Book struct
 type Book struct {
-	ID            primitive.ObjectID `bson:"_id"`
-	ExternalID    string             `bson:"external_id"`
-	Title         string             `bson:"title"`
-	Authors       primitive.A        `bson:"authors"`
-	Description   string             `bson:"description"`
-	Category      string             `bson:"categories"`
-	PublishedDate time.Time          `bson:"published_date"`
-	Thumbnail     string             `bson:"thumbnail"`
-	Deleted       bool               `bson:"deleted"`
-	CreatedAt     time.Time          `bson:"created_at"`
-	UpdatedAt     time.Time          `bson:"updated_at"`
+	// Mongo wil autogenerate objectID _id on insert so we skip adding it here
+	ExternalID    string      `bson:"external_id"`
+	Title         string      `bson:"title"`
+	Authors       primitive.A `bson:"authors"`
+	Description   string      `bson:"description"`
+	Category      string      `bson:"category"`
+	Publisher     string      `bson:"publisher"`
+	PublishedDate string      `bson:"published_date"`
+	ISBN          string      `bson:"isbn"`
+	Thumbnail     string      `bson:"thumbnail"`
+	Deleted       bool        `bson:"deleted"`
+	CreatedAt     time.Time   `bson:"created_at"`
+	UpdatedAt     time.Time   `bson:"updated_at"`
 }
