63ad3d7ac118968d436a3d3409762b14607dd02b
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2021-02-25 14:31:26 +0300

    Better Way to issue token

--- src/api/services/auth/auth.go
+++ src/api/services/auth/auth.go
@@ -7,32 +7,21 @@ import (
 
 	cfg "gofiber-boilerplate/api/configs"
 
-	jwt "gopkg.in/dgrijalva/jwt-go.v3"
+	jwt "github.com/form3tech-oss/jwt-go"
 )
 
 // Claims represents JWT claims
 type Claims struct {
-	ExternalID string `json:"id"`
+	ExternalID string `json:"userID"`
 	Role       string `json:"role"`
 	jwt.StandardClaims
 }
 
 // IssueToken generate tokens used for auth
 func IssueToken(u user.User) (string, error) {
-	nowTime := time.Now()
-	expireTime := nowTime.Add((24 * time.Hour) * 14) // 14 days
+	expireTime := time.Now().Add((24 * time.Hour) * 14) // 14 days
 
-	// Create token
-	// token := jwt.New(jwt.SigningMethodHS256)
-
-	// Set claims
-	// claims := token.Claims.(jwt.MapClaims)
-	// claims["userId"] = u.ExternalID
-	// claims["role"] = u.Role
-	// claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-
-	// Generate encoded token and send it as response.
-
+	// Generate encoded token
 	claims := Claims{
 		u.ExternalID,
 		u.Role,
@@ -43,25 +32,6 @@ func IssueToken(u user.User) (string, error) {
 	}
 
 	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return tokenClaims.SignedString([]byte(cfg.GetConfig().JWTSecret))
-}
-
-// ParseToken parsing token
-func ParseToken(token string) (*Claims, error) {
-	tokenClaims, err := jwt.ParseWithClaims(
-		token,
-		&Claims{},
-		func(token *jwt.Token) (interface{}, error) {
-			return []byte(cfg.GetConfig().JWTSecret), nil
-		},
-	)
-
-	if tokenClaims != nil {
-		claims, ok := tokenClaims.Claims.(*Claims)
-		if ok && tokenClaims.Valid {
-			return claims, nil
-		}
-	}
-
-	return nil, err
+	t, _ := tokenClaims.SignedString([]byte(cfg.GetConfig().JWTSecret))
+	return t, nil
 }
