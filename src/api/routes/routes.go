8a84d7e78842a858c5ce68fe08cdd222a69ccf92
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2021-02-25 14:32:32 +0300

    Move Docs to v route and Guard Create Book route

--- src/api/routes/routes.go
+++ src/api/routes/routes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	// Controllers
 	ctl "gofiber-boilerplate/api/controllers"
+	// Middlewares
+	"gofiber-boilerplate/api/middlewares"
 
 	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
@@ -11,11 +13,11 @@ import (
 // SetupRoutes setups router
 func SetupRoutes(app *fiber.App) {
 
-	app.Use("/docs", swagger.Handler)
-
 	api := app.Group("/api")
 
 	v1 := api.Group("/v1")
+
+	v1.Use("/docs", swagger.Handler)
 
 	v1.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
@@ -31,7 +33,10 @@ func SetupRoutes(app *fiber.App) {
 	auth.Post("/login", ctl.Login)
 
 	// Books
-	v1.Post("/books", ctl.CreateBook)
-	v1.Get("/books", ctl.GetAllBooks)
-	v1.Get("/books/:id", ctl.GetBookByID)
+	books := v1.Group("/books")
+	books.Get("/", ctl.GetAllBooks)
+	books.Get("/:bookID", ctl.GetBookByID)
+
+	// Authenticated Routes
+	books.Post("/", middlewares.RequireLoggedIn(), ctl.CreateBook)
 }
